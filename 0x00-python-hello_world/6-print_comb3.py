f09f24c90396c38e7fb126996d10f4a9505817d7
Chris Stamper <cstampok@gmail.com>
2023-01-06 22:32:29 -0600

    replace . with ,

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -2,6 +2,6 @@
 for diglett in range(0, 10):
     for dugtrio in range(diglett + 1, 10):
         if diglett == 8 and dugtrio == 9:
-            print("{}{}".format(diglett, dugtrio), end="")
+            print("{}{}".format(diglett, dugtrio))
         else:
-            print("{}{}".format(diglett, dugtrio), end=". ")
+            print("{}{}".format(diglett, dugtrio), end=", ")
