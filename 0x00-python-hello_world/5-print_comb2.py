021973d8e995d69f791b0f7927e4cceb11ca3370
Chris Stamper <cstampok@gmail.com>
2023-01-06 22:17:15 -0600

    numlilbug

---
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for numblilbug in range(0, 100):
+    if numblilbug == 99:
+        print("{}".format(numblilbug))
+    else:
+        print("{:02}".format(numblilbug), end=", ")
