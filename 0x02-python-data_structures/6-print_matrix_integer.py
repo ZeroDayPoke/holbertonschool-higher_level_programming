6b089d9218eeb0115ad4944b61969ffee3fa7a7b
Chris Stamper <cstampok@gmail.com>
2023-01-12 17:19:44 -0600

    iterators now iterletter

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            print("{}".format(j), end=" ")
+    for iteri in matrix:
+        for iterj in iteri:
+            print("{:d}".format(iterj), end=" ")
         print()
