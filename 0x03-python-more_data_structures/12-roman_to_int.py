7ff45284ad699eccdd90f2717f4ad91fc1530a64
Chris Stamper <cstampok@gmail.com>
2023-01-12 18:41:38 -0600

    style cannot be blocked

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,11 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    ivxlcdm = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    rom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     int_stor = 0
-    for inti in range(len(roman_string)):
-        if inti > 0 and ivxlcdm[roman_string[inti]] > ivxlcdm[roman_string[inti - 1]]:
-            int_stor += ivxlcdm[roman_string[inti]] - 2 * ivxlcdm[roman_string[inti - 1]]
+    for ii in range(len(roman_string)):
+        if ii > 0 and rom[roman_string[ii]] > rom[roman_string[ii - 1]]:
+            int_stor += rom[roman_string[ii]] - 2 * rom[roman_string[ii - 1]]
         else:
-            int_stor += ivxlcdm[roman_string[inti]]
+            int_stor += rom[roman_string[ii]]
     return int_stor
