9834c963cef2b1c62effa8e42c29a4b632ce7a91
Chris Stamper <cstampok@gmail.com>
2023-01-12 19:22:33 -0600

    fix pep8 style

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -6,9 +6,11 @@ class Square:
     """in new square class uses setter"""
     def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, valx):
         if not isinstance(valx, int):
@@ -16,5 +18,6 @@ class Square:
         elif valx < 0:
             raise ValueError("size must be >= 0")
         self.__size = valx
+
     def area(self):
         return self.__size ** 2
--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -6,9 +6,11 @@ class Square:
     """in moar powerful square square can print square"""
     def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, valx):
         if not isinstance(valx, int):
@@ -16,8 +18,10 @@ class Square:
         elif valx < 0:
             raise ValueError("size must be >= 0")
         self.__size = valx
+
     def area(self):
         return self.__size ** 2
+
     def my_print(self):
         if self.__size == 0:
             print()
