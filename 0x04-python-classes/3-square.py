7f25f0e9e95a3ca98d87f527b5acdbf4626bf747
Chris Stamper <cstampok@gmail.com>
2023-01-12 19:07:27 -0600

    so many squares

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, valx):
+        if not isinstance(valx, int):
+            raise TypeError("size must be an integer")
+        elif valx < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = valx
+    def area(self):
+        return self.__size ** 2
