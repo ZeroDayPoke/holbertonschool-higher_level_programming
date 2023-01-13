c11d4e0ec4c315c27a42f96125320c2f46940739
Chris Stamper <cstampok@gmail.com>
2023-01-12 19:13:26 -0600

    test continues

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Square is a square?"""
 class Square:
     """
     A class named Square... that represents a square
     """
     def __init__(self, size):
+        """Square squares have a size"""
         self.__size = size
