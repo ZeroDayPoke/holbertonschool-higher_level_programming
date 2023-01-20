246f98f87d9c6a8cbe153df3209396d14f768102
Chris Stamper <cstampok@gmail.com>
2023-01-20 14:28:09 -0600

    comment test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """comment test"""
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
