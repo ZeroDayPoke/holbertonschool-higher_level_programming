c69f6849c651be49283184bad7d6996cde801d01
Chris Stamper <cstampok@gmail.com>
2023-01-19 23:53:56 -0600

    oof

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     "sassy classy"
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
