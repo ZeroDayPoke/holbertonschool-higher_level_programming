76e930c47fd5f8c78ea0df5c4c829c1979d119c2
Chris Stamper <cstampok@gmail.com>
2023-01-19 23:42:32 -0600

    rektrecs

---
+++ 0x06-python-more_classes/0-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"R3K7 Rect"
+
+
+class Rectangle:
+    "sassy classy"
+    def __init__(self, width=0, height=0):
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
