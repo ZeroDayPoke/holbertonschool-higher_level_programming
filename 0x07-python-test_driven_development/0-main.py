dd03125ffac349f99206019e11c4c7fa193e23d4
Chris Stamper <cstampok@gmail.com>
2023-01-20 16:52:52 -0600

    numbers too big

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -3,7 +3,7 @@ add_integer = __import__('0-add_integer').add_integer
 
 print(add_integer(1, 2))
 print(add_integer(100, -2))
-print(add_integer(2))
+print(add_integer(2, 922354545454543720368545465465465465465465465465464775808))
 print(add_integer(100.3, -2))
 try:
     print(add_integer(4, "School"))
