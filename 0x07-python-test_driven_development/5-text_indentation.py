19b64ff6ff13abcf30a2852dee0b1eb846df4232
Chris Stamper <cstampok@gmail.com>
2023-01-20 20:03:12 -0600

    fix indent

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,5 +6,6 @@ def text_indentation(text):
     """placeholder"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text = text.replace(".", ".\n\n").replace("?", "?\n\n").replace(":", ":\n\n")
+    text = text.replace(".", ".\n"
+                        "\n").replace("?", "?\n\n").replace(":", ":\n\n")
     print(text, end="")
