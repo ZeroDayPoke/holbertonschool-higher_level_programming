1222d2a1c9bdfac5b3c2906d107ae15bfaf3bb28
Chris Stamper <cstampok@gmail.com>
2023-01-24 19:28:59 -0600

    make ooman readable

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,10 +6,9 @@ def text_indentation(text):
     """placeholder"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text = text.lstrip(' ')
-    text = text.rstrip(' ')
-    text = text.replace(".", ".\n"
-                        "\n").replace("?", "?\n\n").replace(":", ":\n\n")
+    text = text.replace(".", ".\n\n")
+    text = text.replace("?", "?\n\n")
+    text = text.replace(":", ":\n\n")
     tokens = text.split("\n")
     tokens = [token.strip() for token in tokens]
     text = "\n".join(tokens)
