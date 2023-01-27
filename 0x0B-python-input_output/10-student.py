a59bc69f46a922e5582419c97452534f6d8818bb
Chris Stamper <cstampok@gmail.com>
2023-01-26 22:39:31 -0600

    seems close

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""json mod"""
+
+
+class Student:
+    """study class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == list:
+            if all(type(attr) == str for attr in attrs):
+                return {ii: getattr(self, ii) for ii in attrs if hasattr(self, ii)}
+        else:
+            return self.__dict__
