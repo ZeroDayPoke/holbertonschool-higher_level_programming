28ea92ea7204a03db22058fd1e63aba0466287bd
Chris Stamper <cstampok@gmail.com>
2023-01-26 22:44:40 -0600

    noice

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""json mod"""
+
+
+class Student:
+    """study class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == list:
+            if all(type(attr) == str for attr in attrs):
+                return {ii: getattr(self, ii)
+                        for ii in attrs if hasattr(self, ii)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for ky, vl in json.items():
+            setattr(self, ky, vl)
