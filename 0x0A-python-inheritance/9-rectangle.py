2f02bce16d59067a47ff0d0ac8ded3c69b9946f7
Chris Stamper <cstampok@gmail.com>
2023-01-26 20:17:26 -0600

    solid output

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""geometry..."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """da rect subclass of base geo"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        rect_string = "[Rectangle] "
+        rect_string += str(self.__width) + "/" + str(self.__height)
+        return rect_string
