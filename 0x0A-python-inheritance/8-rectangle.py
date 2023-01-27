7bd2160ab528e9d91726f6204c13113014de6f89
Chris Stamper <cstampok@gmail.com>
2023-01-26 19:47:40 -0600

    hmmmmm

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""geometry..."""
+
+
+class BaseGeometry:
+    """UWU"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """da rect subclass of base geo"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
