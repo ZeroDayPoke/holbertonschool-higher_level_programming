163e15b367aa1e229c2027aba1f47ef5ed4cd7b2
Chris Stamper <cstampok@gmail.com>
2023-01-26 22:50:31 -0600

    gg?

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""triangle mod"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    trian = [[1]]
+    for ii in range(1, n):
+        rw = [1]
+        for ij in range(1, ii):
+            rw.append(trian[ii-1][ij-1] + trian[ii-1][ij])
+        rw.append(1)
+        trian.append(rw)
+    return trian
