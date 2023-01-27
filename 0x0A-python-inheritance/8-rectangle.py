3300660724a3bc70cb7c839c794482616b423a64
Chris Stamper <cstampok@gmail.com>
2023-01-26 19:49:50 -0600

    fix pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,14 +3,16 @@
 
 
 class BaseGeometry:
-    """UWU"""
+    """hmmmmmm"""
     def area(self):
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
     """da rect subclass of base geo"""
