e74325fea70779dfd90cf4fb53f3e923cc16f3cf
Chris Stamper <cstampok@gmail.com>
2023-01-28 13:05:08 -0600

    creater setter for x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,3 +30,21 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         self.__height = value
+
+    @property
+    def x(self):
+        """get x property of rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """get y property of rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
