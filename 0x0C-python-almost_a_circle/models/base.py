a6e97d90f0609d400d4bed97250f72321e5bedc5
Chris Stamper <cstampok@gmail.com>
2023-01-28 14:18:18 -0600

    looks good

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """almost a circle module"""
+import json
 
 
 class Base:
@@ -12,3 +13,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
