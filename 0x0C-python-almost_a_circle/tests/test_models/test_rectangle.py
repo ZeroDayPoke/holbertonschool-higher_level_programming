11cc7565bd5d476b6eef30e7a4299b44fc8f8562
Chris Stamper <cstampok@gmail.com>
2023-01-28 19:30:36 -0600

    check checker count

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,8 +7,34 @@ from models.rectangle import Rectangle
 
 class TestRect(unittest.TestCase):
     """da UWUnit tests"""
+
     def test_rekt_base(self):
-        self.assertIsInstance(Rectangle(1, 1), Base)
+        self.assertIsInstance(Rectangle(2, 2), Base)
+
+    def test_no_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle()
+
+    def test_no_height(self):
+        with self.assertRaises(TypeError):
+            Rectangle(4)
+
+    def test_min_args(self):
+        rekt1 = Rectangle(2, 2)
+        self.assertEqual(rekt1.area(), 4)
+
+    def test_multi_rect(self):
+        rekt1 = Rectangle(1, 1)
+        rekt2 = Rectangle(1, 1)
+        self.assertEqual(rekt1.id, rekt2.id - 1)
+
+    def test_arg_limit(self):
+        with self.assertRaises(TypeError):
+            Rectangle(1, 1, 1, 2, 2, 2)
+
+    def test_privacy(self):
+        with self.assertRaises(AttributeError):
+            print(Rectangle(1, 1, 1, 1, 1).__width)
 
 if __name__ == '__main__':
     unittest.main()
