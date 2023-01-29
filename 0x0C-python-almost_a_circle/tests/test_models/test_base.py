9756952a1f4ae24d503856a288f3efdf25b95225
Chris Stamper <cstampok@gmail.com>
2023-01-28 20:12:05 -0600

    moar teztz

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,9 +6,41 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     """da UWUnit tests"""
-    def test_no_arg(self):
-        base1 = Base()
-        self.assertEqual(base1.id, 1)
+    def test_non(self):
+        base1 = Base(None)
+        base2 = Base()
+        self.assertEqual(base1.id + 1, base2.id)
+
+    def test_int(self):
+        self.assertEqual(Base(1).id, 1)
+
+    def test_flt(self):
+        self.assertEqual(Base(1.1).id, 1.1)
+
+    def test_chr(self):
+        self.assertEqual(Base('a').id, 'a')
+
+    def test_str(self):
+        self.assertEqual(Base("hi").id, "hi")
+
+    def test_boo(self):
+        self.assertEqual(Base(True).id, True)
+
+    def test_lst(self):
+        self.assertEqual(Base([1, 1]).id, [1, 1])
+
+    def test_dic(self):
+        self.assertEqual(Base({"richard": 1}).id, {"richard": 1})
+
+    def test_tup(self):
+        self.assertEqual(Base((1, 1)).id, (1, 1))
+
+    def test_set(self):
+        self.assertEqual(Base({1, 1}).id, {1, 1})
+
+    def test_privacy_1(self):
+        with self.assertRaises(AttributeError):
+            print(Base.__nb_objects)
 
 if __name__ == '__main__':
     unittest.main()
