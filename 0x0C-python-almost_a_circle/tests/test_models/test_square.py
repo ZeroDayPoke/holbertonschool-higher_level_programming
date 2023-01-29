febdb64f795829ba92fa5a240c40804c232a7e9a
Chris Stamper <cstampok@gmail.com>
2023-01-28 20:52:20 -0600

    great sadness

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -13,5 +13,49 @@ class TestBase(unittest.TestCase):
     def test_if_rekt(self):
         self.assertIsInstance(Square(1), Rectangle)
 
+    def test_flt(self):
+        with self.assertRaises(TypeError):
+            Square(2.2)
+
+    def test_non(self):
+        with self.assertRaises(TypeError):
+            Square(None)
+
+    def test_chr(self):
+        with self.assertRaises(TypeError):
+            Square('a')
+
+    def test_str(self):
+        with self.assertRaises(TypeError):
+            Square("UWU")
+
+    def test_boo(self):
+        with self.assertRaises(TypeError):
+            Square(True)
+
+    def test_lst(self):
+        with self.assertRaises(TypeError):
+            Square([2, 2])
+
+    def test_dic(self):
+        with self.assertRaises(TypeError):
+            Square({"size": 1})
+
+    def test_set(self):
+        with self.assertRaises(TypeError):
+            Square({1, 1})
+
+    def test_tup(self):
+        with self.assertRaises(TypeError):
+            Square((1, 1))
+
+    def test_ngv(self):
+        with self.assertRaises(ValueError):
+            Square(-3)
+
+    def test_zed(self):
+        with self.assertRaises(ValueError):
+            Square(0)
+
 if __name__ == '__main__':
     unittest.main()
