e9df8aeefb0c3a009e19fc4a39929d4ae0b253af
Chris Stamper <cstampok@gmail.com>
2023-01-28 21:02:01 -0600

    so square

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -57,5 +57,51 @@ class TestBase(unittest.TestCase):
         with self.assertRaises(ValueError):
             Square(0)
 
+    def test_update_1(self):
+        sqr1 = Square(1, 1, 1, 1)
+        sqr1.update(2)
+        self.assertEqual(sqr1.id, 2)
+
+    def test_update_2(self):
+        sqr1 = Square(1, 1, 1, 1)
+        sqr1.update(2, 2)
+        self.assertEqual(sqr1.size, 2)
+
+    def test_update_3(self):
+        sqr1 = Square(1, 1, 1, 1)
+        sqr1.update(2, 2, 2)
+        self.assertEqual(sqr1.x, 2)
+
+    def test_update_4(self):
+        sqr1 = Square(1, 1, 1, 1)
+        sqr1.update(2, 2, 2, 2)
+        self.assertEqual(sqr1.y, 2)
+
+    def test_upd_kwa_1(self):
+        sqr1 = Square(1, 1, 1, 1)
+        sqr1.update(id=2)
+        self.assertEqual(sqr1.id, 2)
+
+    def test_upd_kwa_2(self):
+        sqr1 = Square(1, 1, 1, 1)
+        sqr1.update(size=2)
+        self.assertEqual(sqr1.size, 2)
+
+    def test_upd_kwa_3(self):
+        sqr1 = Square(1, 1, 1, 1)
+        sqr1.update(x=2)
+        self.assertEqual(sqr1.x, 2)
+
+    def test_upd_kwa_4(self):
+        sqr1 = Square(1, 1, 1, 1)
+        sqr1.update(y=2)
+        self.assertEqual(sqr1.y, 2)
+
+    def test_str_sqr(self):
+        sqr1 = Square(1, 1, 1, 1)
+        sqrstr1 = sqr1.__str__()
+        sqrstr2 = '[Square] (1) 1/1 - 1'
+        self.assertEqual(sqrstr1, sqrstr2)
+
 if __name__ == '__main__':
     unittest.main()
