1fecd416dd6c0b20001193deea2c67880e457851
Chris Stamper <cstampok@gmail.com>
2023-02-10 23:53:55 -0600

    test

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- ASC ORD CITYID
 SELECT city.id, city.name, stat.name
-FROM cities AS city
-    INNER JOIN states AS stat
+FROM 'cities' AS city
+    INNER JOIN 'states' AS stat
     ON city.state_id = state.id
-ORDER BY city.id ASC;
+ORDER BY city.id;
