d09382ddc45fba26e5ea16f33de51953eaa34ab2
ZeroDayPoke <cstampok@gmail.com>
2023-02-24 21:06:47 -0600

    gg

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""test comment"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    [print(state) for state in cur.fetchall() if state[1][0] == "N"]
