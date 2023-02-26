5bd7e5ee6b6f66dd5a219146021841abc6590622
CStamper <cstampok@gmail.com>
2023-02-25 19:41:18 -0600

    solid local

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.sql
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.sql
@@ -1,2 +1,3 @@
 -- Insert states
+USE hbtn_0e_6_usa;
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
---
+++ 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""test comment"""
+import sys
+from sqlalchemy import create_engine
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(state.id, state.name))
