da766f17ff30d7228e346d39f1263fbbc9baa4df
CStamper <cstampok@gmail.com>
2023-02-25 20:02:30 -0600

    hmmmm

---
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""test comment"""
+import sys
+from sqlalchemy import create_engine
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    toggle = False
+    for state in session.query(State):
+        if sys.argv[4] == state.name:
+            toggle = True
+            print("{}".format(state.id))
+    if toggle == False:
+        print("Not found")
---
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""test comment"""
+import sys
+from sqlalchemy import create_engine
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    toggle = False
+    for state in session.query(State):
+        if state.name == "Louisiana":
+            toggle = True
+            break
+    if toggle == True:
+        print("LA already exists in db")
+    else:
+        LA = State(name="Louisiana")
+        session.add(LA)
+        session.commit()
+        print(LA.id)
