6374e7a66aeffca8c2d854c40c43a7276eab469c
CStamper <cstampok@gmail.com>
2023-02-25 20:35:31 -0600

    add safe

--- 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,4 +12,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(state.id, state.name))
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
