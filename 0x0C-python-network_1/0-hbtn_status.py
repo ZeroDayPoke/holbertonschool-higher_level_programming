cd69b5d041f765c4a5c4f04c65521b2cbbd913ba
CStamper <cstampok@gmail.com>
2023-03-10 23:46:56 -0600

    update

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -4,10 +4,10 @@ import urllib.request
 
 
 if __name__ == "main":
-    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as resp:
         body = resp.read()
         print("Body response:")
-        print("type: {}".format(type(body)))
-        print("content: {}".format(body))
-        print("utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
