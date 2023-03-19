6517a5c9032c9b57e365347b0f31a4843b441f82
CStamper <cstampok@gmail.com>
2023-03-19 05:49:33 -0500

    COMPLETE

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+Write a function that prints the numbers from 1 to 100 separated by a space.
+
+For multiples of three print Fizz instead of the number and for multiples of five print Buzz.
+For numbers which are multiples of both three and five print FizzBuzz.
+Prototype: def fizzbuzz():
+Each element should be followed by a space
+You are not allowed to import any module
+You don’t need to understand __import__
+"""
 def fizzbuzz():
     for numblilbug in range(1, 101):
         if numblilbug % 15 == 0:
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+Write a function that prints a string in uppercase followed by a new line.
+
+Prototype: def uppercase(str):
+You can only use no more than 2 print functions with string format
+You can only use one loop in your code
+You are not allowed to import any module
+You are not allowed to use str.upper() and str.isupper()
+Tips: ord()
+You don’t need to understand __import__
+"""
 def uppercase(str):
     for char in str:
         if ord(char) > 96 and ord(char) < 123:
