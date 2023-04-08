654525cbd2019d00f51b3b0e4cf2e60cd8df132f
CStamper <cstampok@gmail.com>
2023-04-07 23:37:47 -0500

    semistandard

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const messages = [
-  "C is fun",
-  "Python is cool",
-  "JavaScript is amazing"
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 for (const message of messages) {
