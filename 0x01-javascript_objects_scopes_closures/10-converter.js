eed2be8626a748159003cb728b247d9abf1adfc2
CStamper <cstampok@gmail.com>
2023-04-08 00:13:59 -0500

    task balance

---
+++ 0x01-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.converter = function (base) {
+  if (base < 2 || base > 36) {
+    return null;
+  }
+  
+  return function (number) {
+    return number.toString(base);
+  };
+};
---
+++ 0x01-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
