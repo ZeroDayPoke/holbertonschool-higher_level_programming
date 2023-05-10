8033cece08fd3d5120a2ad47b236d9a79821b0bf
ZeroDayPoke <cstampok@gmail.com>
2023-05-10 05:19:18 -0500

    test

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 """
-Write a function that prints a string in uppercase followed by a new line.
-
-Prototype: def uppercase(str):
-You can only use no more than 2 print functions with string format
-You can only use one loop in your code
-You are not allowed to import any module
-You are not allowed to use str.upper() and str.isupper()
-Tips: ord()
-You don’t need to understand __import__
+Checking if a character is uppercase or lowercase
 """
 
 
