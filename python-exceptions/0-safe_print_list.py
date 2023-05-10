4fc89dfdc3f2958a6503f0e791c0555235e26726
ZeroDayPoke <cstampok@gmail.com>
2023-05-10 05:45:05 -0500

    gg

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(my_list[i], end=' ')
+            print(my_list[i], end='')
             count += 1
         print()
     except IndexError:
