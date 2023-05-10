5dc474a67bf196067f8d2d84ee2fc39b0684b626
ZeroDayPoke <cstampok@gmail.com>
2023-05-10 05:03:30 -0500

    test

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Write a function that prints the numbers from 1 to 100 separated by a space.
+
+For multiples of three print Fizz instead of the number and for multiples of five print Buzz.
+For numbers which are multiples of both three and five print FizzBuzz.
+Prototype: def fizzbuzz():
+Each element should be followed by a space
+You are not allowed to import any module
+You don’t need to understand __import__
+"""
+def fizzbuzz():
+    for numblilbug in range(1, 101):
+        if numblilbug % 15 == 0:
+            print("FizzBuzz ", end="")
+        elif numblilbug % 3 == 0:
+            print("Fizz ", end="")
+        elif numblilbug % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(numblilbug), end="")
