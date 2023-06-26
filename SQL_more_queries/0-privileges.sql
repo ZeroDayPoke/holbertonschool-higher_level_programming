9ddbf4151de633a24945f450e581042c89a89d26
ZeroDayPoke <cstampok@gmail.com>
2023-06-26 17:23:14 -0500

    test

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- SHOW GRANTS PRACTICE
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT CONCAT('Grants for ', user, '@', host) AS 'Grants'
+FROM mysql.user
+WHERE user IN ('user_0d_1', 'user_0d_2')
+  AND host = 'localhost';
