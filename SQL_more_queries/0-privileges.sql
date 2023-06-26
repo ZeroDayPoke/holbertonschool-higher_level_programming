1ed101cadf22024843a97198bd63a397a4ac335e
ZeroDayPoke <cstampok@gmail.com>
2023-06-26 17:35:20 -0500

    test

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
--- new test
-SELECT * FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2');
+-- SHOW GRANTS PRACTICE
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
