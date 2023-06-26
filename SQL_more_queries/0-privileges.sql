54f253d01479bd61f65234e5091f31d5ed05fd22
ZeroDayPoke <cstampok@gmail.com>
2023-06-26 17:28:18 -0500

    test

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,2 @@
--- SHOW GRANTS PRACTICE
-SELECT CONCAT('Grants for ', user, '@', host) AS 'Grants'
-FROM mysql.user
-WHERE user IN ('user_0d_1', 'user_0d_2')
-  AND host = 'localhost';
+-- new test
+SELECT * FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2');
